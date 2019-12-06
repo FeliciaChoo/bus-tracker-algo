fec293f1178297fea73c80daef6ebaa3fbf0ace2
Paulo Valadares <paulohva@gmail.com>
2019-12-06 15:35:11 +0000

    import script improvements

--- mongo-seed/import.sh
+++ mongo-seed/import.sh
@@ -1,2 +1,6 @@
-#! /bin/bash
-mongoimport --host=mongo -d busTracker -c dublinBusGPSSample --type csv --file /mongo-seed/dublin-bus-sample.csv --fields timestamp,lineID,direction,journeyPatternID,timeFrame,vehicleJourneyID,operator,congestion,lonWGS84,latWGS84,delay,blockID,vehicleID,stopID,atStop
+#!/bin/bash
+if [ ! -f "/mongo-seed/dublin-bus-sample.csv" ]; then
+  echo "The file 'dublin-bus-sample.csv' does not exist. Skiping mongo-seed task."
+else
+  mongoimport --host=mongo -d busTracker -c dublinBusGPSSample --type csv --file /mongo-seed/dublin-bus-sample.csv --fields timestamp,lineID,direction,journeyPatternID,timeFrame,vehicleJourneyID,operator,congestion,lonWGS84,latWGS84,delay,blockID,vehicleID,stopID,atStop
+fi
