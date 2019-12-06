3185b47de0e592d358560680e69f782833cec1b2
Paulo Valadares <paulohva@gmail.com>
2019-12-06 15:25:05 +0000

    mongo-seed configuration for docker-compose

---
+++ mongo-seed/import.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+mongoimport --host=mongo -d busTracker -c dublinBusGPSSample --type csv --file /mongo-seed/dublin-bus-sample.csv --fields timestamp,lineID,direction,journeyPatternID,timeFrame,vehicleJourneyID,operator,congestion,lonWGS84,latWGS84,delay,blockID,vehicleID,stopID,atStop
